4bcff137cbb738b870ab88fe081dc00473f459ba
james <james@jamesfinnerty.co.uk>
2021-02-11 17:00:57 +0000

    Add project files.

---
+++ InfoTrackTest/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using InfoTrackTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoTrackTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [HttpPost]
+        public IActionResult ExecuteSeach(SearchFormModel search)
+        {
+
+            string siteUrl = "https://www.google.co.uk/search?num=100&q=";
+            string searchTerms = search.SearchTerms;
+
+            // format terms for google query
+            searchTerms = searchTerms.Replace(' ', '+');
+
+            string divStarter = "<div class=\"ZINbbc xpd O9g5cc uUPGi\"><div class=\"kCrYT\">";
+            int resultCount = 100;
+
+            System.Net.WebClient webClient = new System.Net.WebClient();
+            byte[] rawPage = webClient.DownloadData(siteUrl + searchTerms);
+            string pageHtml = System.Text.Encoding.UTF8.GetString(rawPage);
+
+            // get the index of the first occurance of the start of Google result HTML
+            var resultsHtml = new List<String>();
+
+            // find index for start of first organic result
+            var firstResultIndex = pageHtml.IndexOf(divStarter);
+
+            // trim preceeding data
+            pageHtml = pageHtml.Substring(firstResultIndex);
+
+            for (int i = 0; i < resultCount; i++)
+            {
+                // find start of second result div by offsetting from the start of the string
+                var workingIndex = pageHtml.IndexOf(divStarter, 1);
+
+                if (workingIndex == -1)
+                {
+                    // handle final reult case
+                    resultsHtml.Add(pageHtml);
+                }
+                else
+                {
+                    // handle usual case
+                    var targetDiv = pageHtml.Substring(0, workingIndex);
+                    resultsHtml.Add(targetDiv);
+
+                    // trim current div off of the start of the file
+                    pageHtml = pageHtml.Substring(workingIndex);
+                }
+            }
+
+            var output = new StringBuilder();
+
+            for(int i = 0; i < resultsHtml.Count; i++)
+            {
+                if(resultsHtml[i].Contains(search.TargetURL))
+                {
+                    output.Append(i.ToString() + " ");
+                }
+            }
+
+            if(output.ToString() == "")
+            {
+                output.Append("0");
+            }
+
+            ViewBag.SearchResult = output.ToString();
+            return View("Index");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InfoTrackTest/Models/SearchFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfoTrackTest.Models
+{
+    public class SearchFormModel
+    {
+        public string SearchTerms { get; set; }
+        public string TargetURL { get; set; }
+    }
+}
